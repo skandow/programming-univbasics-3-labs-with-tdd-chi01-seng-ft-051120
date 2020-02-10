b17f8feaf7ea7c4d2cdd034be7995005de9f7aaa
Steven Kandow <kando1sr@gmail.com>
2020-02-10 11:18:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 16
 second_number = 17
 sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
